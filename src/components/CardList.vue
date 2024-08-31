f192f1d40ed78e178f247db354bb3a647c0ca542
Пользователь <Kalichak_eo@mail.ru>
2024-08-31 22:40:57 +0500

    Add files

---
+++ src/components/CardList.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Card from './Card.vue'
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      title="Мужской кроссовок Nike Blazer Mid Suede"
+      image-url="/sneakers/sneakers-1.jpg"
+      :price="5000"
+      :is-favorite="false"
+    />
+  </div>
+</template>
+
+<!--2:14:45-->
