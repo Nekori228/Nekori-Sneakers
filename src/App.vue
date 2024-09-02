168033556964cce95381835be8c1c0516eedd2ba
Пользователь <Kalichak_eo@mail.ru>
2024-09-02 11:27:27 +0500

    Update api request

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, reactive, ref, watch } from 'vue'
 import axios from 'axios'
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
@@ -7,15 +7,58 @@ import Drawer from './components/Drawer.vue'
 
 const items = ref([])
 
-onMounted(async () => {
-  try {
-    const { data } = await axios.get('https://27f08552a729a4dd.mokky.dev/items')
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: ''
+})
 
-    items.value = data
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+
+const onChangeSearchInput = (event) => {
+  filters.searchQuery = event.target.value
+}
+
+const fetchFavorites = async () => {
+  try {
+    const { data: favorites } = await axios.get(`https://27f08552a729a4dd.mokky.dev/favorites`)
+
+    items.value = item.value.map((item) => {
+      const favorite = favorites.find((favorite) => favorite.id === item.id)
+    })
   } catch (err) {
     console.log(err)
   }
+}
+
+const fetchItems = async () => {
+  try {
+    const params = {
+      sortBy: filters.sortBy
+    }
+
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
+    }
+
+    const { data } = await axios.get(`https://27f08552a729a4dd.mokky.dev/items`, { params })
+
+    items.value = data.map((obj) => ({
+      ...obj,
+      isFavorite: false,
+      isAdded: false
+    }))
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+onMounted(async () => {
+  await fetchItems()
+  await fetchFavorites()
 })
+watch(filters, fetchItems)
 </script>
 
 <template>
@@ -28,15 +71,16 @@ onMounted(async () => {
         <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
 
         <div class="flex gap-4">
-          <select class="py-2 px-3 border rounded-md outline-none">
-            <option>По названию</option>
-            <option>По цене (дешёвые)</option>
-            <option>По цене (дорогие)</option>
+          <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none">
+            <option value="name">По названию</option>
+            <option value="price">По цене (дешёвые)</option>
+            <option value="-price">По цене (дорогие)</option>
           </select>
 
           <div class="relative">
             <img class="absolute left-4 top-3" src="/search.svg" alt="search" />
             <input
+              @input="onChangeSearchInput"
               class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
               type="text"
               placeholder="Поиск..."
